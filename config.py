dc3dbcd9a81f7a98f13325488dc62245767c511e
Shoukolate <karanvir.singh61@yahoo.in>
2020-04-15 18:05:48 +0530

    Do not write anything to /

--- config.py
+++ config.py
@@ -1,15 +1,10 @@
 import os
 
-out=os.system('touch /testforclouddecrypter')
-if out!=0:
-	print("Please run as root")
-	exit()
+if os.geteuid()==0:
+	print "Please run as root"
+	print("Installing Necessary package....")
+	os.system("apt install rclone gpg")
+	print("\nInstalling pyfiglet...")
+	os.system("pip3 install pyfiglet")
 else:
-	print('working fine')
-	os.system('rm /testforclouddecrypter')
-
-print("Installing Necessary package....")
-os.system("apt install rclone gpg")
-
-print("\nInstalling pyfiglet...")
-os.system("pip3 install pyfiglet")
+	print "working fine"
